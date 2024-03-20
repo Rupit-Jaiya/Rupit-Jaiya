f3c4ffc3ec8402eede5bd77994ce3280e8d42e20
Rupit-Jaiya <159528153+Rupit-Jaiya@users.noreply.github.com>
2024-03-20 17:17:03 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+// it cannot be reverted ^_^
+
+//alert(3+3)we are using nodejs, not browser ^_^
+console.log(1+1); console.log("Rupit");
+//the above statement is not very much readable
+// use different lines for code to make it more readable
+//for Documentation 
+// TC39 and MDN
+let name = "Rupit";
+let age = 20
+let isLoggedIn = false
+
+// number and it has a particular range 
+// bigint
+// string => ""
+// boolean => false/true
+// null => it is a standalong value it is a data type
+// null is a representation of an empty value
+// undefined => it is when you haven't defined a value
+// sumbol => unique
+
+// object
+
+console.log(typeof 2)
+console.log(typeof null)
+console.log(typeof undefined)
